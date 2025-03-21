042e44c4a6d6289c75ef9408b3fdafa47c087e74
Netanel Hazan <jzi770@gmail.com>
2025-03-21 17:01:40 +0200

    other way to implement

--- mession-1.js
+++ mession-1.js
@@ -1,10 +1,5 @@
 // submit form and add product
-const tableProduct = {};
-
-tableProduct.name =  new Array();
-tableProduct.price = new Array();
-tableProduct.category = new Array();
-tableProduct.urlImage = new Array();
+const tableProduct = [];
 
 
 let nameProduct = document.querySelector('#name-product');
@@ -12,8 +7,11 @@ let price = document.querySelector('#price-product');
 let category = document.querySelector('#choose-product');
 let urlImage = document.querySelector('#url-image');
 
-listProduct = [nameProduct, price, category];
+const form = document.querySelector('form');
 let table = document.querySelector('table');
+let tbody = table.querySelector('tbody');
+
+listProduct = [nameProduct, price, category];
 let counter = 0;
 
 function addProduct(e) {
@@ -22,29 +20,36 @@ function addProduct(e) {
         alert('please fill all field')
         return;
     }
-    // console.log('add product check');
-    // console.log(tableProduct);
-    console.log(e);
     
-    tableProduct.name.push(nameProduct.value);
-    tableProduct.price.push(price.value);
-    tableProduct.category.push(category.value);
-    tableProduct.urlImage.push(urlImage.value);
+    tableProduct.push({
+        name: nameProduct.value,
+        price: price.value,
+        category: category.value,
+        urlImage: urlImage.value
+    });
 
     // add to table html
-    let tbody = table.querySelector('tbody');
     let tr = document.createElement('tr');
-    for (let i = 0; i < listProduct.length; i++) {
+    let product = tableProduct[counter];
+    console.log(product);
+
+    let keys = Object.keys(product);
+    console.log(keys);
+    
+
+    for (let i = 0; i < keys.length-1; i++) {
         let td = document.createElement('td');
-        td.textContent = listProduct[i].value;
+        td.textContent = product[keys[i]];
         tr.appendChild(td);
     }
+    // handle image
     let img = document.createElement('img');
     td = document.createElement('td');
-    img.src = urlImage.value;
+    img.src = product.urlImage;
     td.appendChild(img);
     tr.appendChild(td);
 
+    // handle delete button
     td = document.createElement('td');
     let button = document.createElement('button');
     button.textContent = 'Delete';
@@ -56,12 +61,8 @@ function addProduct(e) {
     tbody.appendChild(tr);
     table.appendChild(tbody);
 
-
-    nameProduct.value = '';
-    price.value = '';
-    category.value = '';
-    urlImage.value = '';
     counter++;
+    form.reset();
 }
 
 function deleteProduct(e) {
@@ -69,5 +70,34 @@ function deleteProduct(e) {
     let index = e.target.parentElement.parentElement.id;
     let tr = document.getElementById(index);
     tr.remove();
-    
+
 }
+
+
+
+
+
+
+/// here how to use inner html to display data on safe way 
+// function sanitizeInput(input) {
+//     return input.replace(/&/g, "&amp;")
+//                 .replace(/</g, "&lt;")
+//                 .replace(/>/g, "&gt;")
+//                 .replace(/"/g, "&quot;")
+//                 .replace(/'/g, "&#39;");
+// }
+
+// Example:
+// let userInput = document.getElementById("check-input").value;
+// let safeInput = sanitizeInput(userInput);
+// console.log(safeInput);
+// document.getElementById("check").innerHTML = safeInput;
+
+// // or use DOMPurify https://cdnjs.cloudflare.com
+// function addInput() {
+//     let userInput = document.getElementById("check-input").value;
+//     console.log(userInput);
+//     let safeInput = DOMPurify.sanitize(userInput);
+//     console.log(safeInput);
+//     document.getElementById("check").innerHTML = safeInput;
+// }
